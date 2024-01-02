c48074f246621da5e261b5c84f1c7e1a81c9b3f2
Paul Zörb <paulzoerb09@icloud.com>
2024-01-02 17:13:24 +0100

    remove_background verbessert

--- remove_background.py
+++ remove_background.py
@@ -1,25 +1,36 @@
+import concurrent.futures
 from rembg import remove
 import os
 import shutil
 
-def remove_backgrounds(input_folder, output_folder):
+def remove_background(file_path, output_folder):
+    if file_path.endswith('.png') or file_path.endswith('.jpg'):
+        output_path = os.path.join(output_folder, os.path.basename(file_path))
+
+        # Entferne den Hintergrund des Bildes
+        with open(file_path, 'rb') as input_image:
+            bg_removed_image = remove(input_image.read())
+
+        # Speichere das Bild mit entferntem Hintergrund im output_folder
+        with open(output_path, 'wb') as output_image:
+            output_image.write(bg_removed_image)
+
+def process_images(input_folder, output_folder):
+    # Erstellen Sie den Ausgabeverzeichnis, falls es noch nicht existiert
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    for filename in os.listdir(input_folder):
-        if filename.endswith('.png') or filename.endswith('.jpg'):
-            input_path = os.path.join(input_folder, filename)
-            output_path = os.path.join(output_folder, filename)
+    # Durchsuchen Sie den Eingabeordner nach allen Dateien
+    file_paths = [os.path.join(input_folder, file) for file in os.listdir(input_folder)
+                 if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
 
-            # Entferne den Hintergrund des Bildes
-            with open(input_path, 'rb') as input_image:
-                bg_removed_image = remove(input_image.read())
+    # Verarbeiten Sie alle Bilder gleichzeitig
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(lambda file_path: remove_background(file_path, output_folder), file_paths)
 
-            # Speichere das Bild mit entferntem Hintergrund im output_folder
-            with open(output_path, 'wb') as output_image:
-                output_image.write(bg_removed_image)
+                
 
-remove_backgrounds("images/with_background/train/dogs", "images/removed_background/train/dogs")
-remove_backgrounds("images/with_background/train/wolves", "images/removed_background/train/wolves")
-remove_backgrounds("images/with_background/test/dogs", "images/removed_background/test/dogs")
-remove_backgrounds("images/with_background/test/wolves", "images/removed_background/test/wolves")
+process_images("images/with_background/train/dogs", "images/removed_background/train/dogs")
+process_images("images/with_background/train/wolves", "images/removed_background/train/wolves")
+process_images("images/with_background/test/dogs", "images/removed_background/test/dogs")
+process_images("images/with_background/test/wolves", "images/removed_background/test/wolves")
