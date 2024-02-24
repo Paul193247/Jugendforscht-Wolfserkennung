7fcf8bf6e204145545297782064a62d5b5cf20b0
Paul Zörb <paulzoerb09@icloud.com>
2024-02-24 13:48:58 +0100

    Änderungen

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Flatten, Dense, Conv2D, MaxPooling2D
 import matplotlib.pyplot as plt
 from sendmessage import sendmessage
+from  tensorflow.keras.callbacks import ModelCheckpoint
 
 # Bildverarbeitungs-Generators
 train_datagen = ImageDataGenerator(
@@ -26,7 +27,7 @@ model = Sequential([Conv2D(30, (3, 3), activation='relu', input_shape=(256, 256,
                     Conv2D(64, (3, 3), activation='relu'),
                     MaxPooling2D(2, 2), 
                     Flatten(),
-                    Dense(750, activation='relu'),
+                    Dense(1000, activation='relu'),
                     Dense(2, activation='softmax')])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
@@ -42,15 +43,25 @@ train_generator = train_datagen.flow_from_directory(
     batch_size=32,
     class_mode='binary')
 
+print(train_generator.class_indices)
+
 # Generieren und laden des Test-Datasets
 test_generator = test_datagen.flow_from_directory(
     'images/edges/test',
     target_size=(256, 256),
     batch_size=32,
-    class_mode='binary')
+    class_mode='binary') 
+
+checkpoint_path = "model/cp.ckpt"
+
+# Create a callback that saves the model's weights
+cp_callback = ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
 
 # Trainieren des Modells
-history = model.fit(train_generator, epochs=40, validation_data=test_generator)
+history = model.fit(train_generator, epochs=60, validation_data=test_generator)
 
 def plot_accuracy_vs_training_data(history):
     acc = history.history['accuracy']
